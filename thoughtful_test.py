71542e35e8b7f04b437027950de9e75a857f3518
hdxestudart <erico.studart@hashdex.com>
2025-08-19 17:03:35 -0300

    first commit

---
+++ thoughtful_test.py
@@ -0,0 +1,30 @@
+import math
+
+
+
+def is_bulky(volume: int, dimensions: list) -> bool:
+    return volume >= 1000000 or any(dimension >= 150 for dimension in dimensions)
+
+def is_heavy(mass: int):
+    return mass >= 20
+
+def get_stack_type(volume: int, dimensions: list[int], mass: int) -> str:
+    if is_bulky(volume, dimensions) and is_heavy(mass):
+        return "REJECTED"
+    elif is_bulky(volume, dimensions) or is_heavy(mass):
+        return "SPECIAL"
+    else:
+        return "STANDARD"
+
+def sort(width: int, height: int, lenght: int, mass: int):
+    dimensions = [width, height, lenght]
+    volume = math.prod(dimensions)
+
+    stack_type = ""
+
+    try:
+        stack_type = get_stack_type(volume, dimensions, mass)
+    except Exception as err:
+        print(f"Could not get stack type, reason: {err}")
+    
+    return stack_type
